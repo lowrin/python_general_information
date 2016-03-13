ff7ea976d126aa84da3ffd67c8aa9b5189489899
Jonas <mail@jonaspfeifer.de>
2016-03-13 15:18:20 +0100

    Update matplotlib_example.py

--- matplotlib_example.py
+++ matplotlib_example.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sqlalchemy import create_engine
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.style.use('seaborn-whitegrid')
 
 
 engine = create_engine("postgresql://postgres:<password>@localhost:5432/<dbname>")
@@ -8,4 +10,10 @@ df = pd.read_excel("b.xlsx")
 print(df.describe())
 print(df.info())
 bp = df.boxplot(return_type='axes')
+
+plt.xlabel('x label')
+plt.ylabel('y label')
+
+plt.title("Simple Plot")
+
 plt.show()
