a3b01f9267e89d7e2e48251cd65e8ec1bd06a327
Jonas <mail@jonaspfeifer.de>
2016-03-13 14:29:21 +0100

    Create dataframe_basiscs.py

---
+++ dataframe_basiscs.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+# creation
+# create df with the given columns
+df = pd.DataFrame(np.nan, index=[], columns=['Source','Target','Label','Weight'])
+
+# create totally emtpry dataframe
+pd.DataFrame(np.nan, index=[], columns=[])
+
+# fill data in
+
+# set cell, no matter if the row or column exists, it will be created
+df.set_value(column,row,value)
+
+
+
+# iteration
+# iterate over dataframe
+for index, row in df.iterrows():
+
+# access rows
+row["column_name"]
+
+
+
+
+# slicing
+df_outer = df[0:1]
+
+
+# print
+
+print(b.describe())
+print(b.info())
+# print full dataframe
+print(b)
